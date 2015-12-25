20bb984868660359c01670f9363eb49d7564864e
hassansin <mhkr63@gmail.com>
2015-12-26 05:14:19 +0600

    added coverage module

---
+++ test/record.js
@@ -0,0 +1,49 @@
+/*
+Records nock definitions if not available
+*/
+
+var nock = require('nock');
+var path = require('path');
+var fs = require('fs-extra');
+
+var nock = require('nock');
+
+module.exports = function (fixture_name){
+	//definition path
+	var fixture_path = path.join( __dirname, 'fixtures',fixture_name + '.json');
+	var has_fixture;
+
+	before(function(done){
+		//check if definition exists and readable
+		fs.access(fixture_path, fs.R_OK | fs.R_OK, function(err){			
+			if(err){
+				//not found, start recording
+				nock.recorder.rec({
+			    output_objects:  true,
+			    dont_print:      true
+			  });				
+			}
+			else{
+				//definitions found, load them
+				has_fixture = true;		
+				nock.load(fixture_path);						
+			}
+			return done();
+		});
+	});
+
+	after(function(done){
+		if(has_fixture){
+			return done();
+		}
+
+		//suite finished, save the recorded definitions
+		fixtures = nock.recorder.play();
+		fs.ensureFile(fixture_path, function(err){
+			if(err){
+				done(err);
+			}
+			fs.writeFile(fixture_path, JSON.stringify(fixtures, null, 2), done);			
+		});
+;	});
+};
---
+++ test/test-coverage.js
@@ -0,0 +1,58 @@
+var errors = require('../lib/errors');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var record_or_play = require('./record');
+
+var coverage = require('../lib/models/coverage');
+var Config = require('../lib/http/config');
+
+
+var config;
+var Coverage;
+
+describe('Coverage', function(){
+
+  before(function() {
+    config = new Config({
+      apiKey: 'n5Cddnj2KST6YV9J2l2ztQQ2VrdPfzA4JPbn',
+      isTest: true
+    });
+    Coverage = coverage(config);
+  });
+
+  describe('#all', function(){
+
+    record_or_play('coverage/all');
+
+    it('exists as public method on Coverage', function(){
+      expect(Coverage).to.have.property('all');
+    });
+
+    it('should throw InvalidRequestError when no param is passed', function(done){
+      Coverage.all()
+        .catch(function(e){
+          expect(e).to.be.an.instanceOf(errors.InvalidRequestError);
+          done();
+        });
+    });
+    
+    it('should fetch coverage', function(done){
+      Coverage.all({
+        "payer_id":"00001",
+        "provider_last_name":"Doe",
+        "provider_first_name":"John",
+        "provider_npi":"0123456789",
+        "member_id":"ZZZ445554301",
+        "member_first_name":"IDA",
+        "member_last_name":"FRANKLIN",
+        "member_dob":"1701-12-12",
+        "service_type":"30"
+      })
+      .then(function(coverage){
+        expect(coverage).to.be.an.instanceOf(Coverage);
+        done();
+      });
+    });
+    
+  });
+});
