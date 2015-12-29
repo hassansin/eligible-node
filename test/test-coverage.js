4ee000e13c64eb293f2fb64f6e2cf935b7ec6eb9
hassansin <mhkr63@gmail.com>
2015-12-29 22:57:28 +0600

    fix whitepacing to conform node coding style

--- test/test-coverage.js
+++ test/test-coverage.js
@@ -10,50 +10,50 @@ var Config = require('../lib/http/config');
 var config;
 var Coverage;
 
-describe('Coverage', function(){
+describe('Coverage', function() {
 
   before(function() {
     config = new Config({
       apiKey: 'n5Cddnj2KST6YV9J2l2ztQQ2VrdPfzA4JPbn',
       isTest: true,
-    });    
+    });
     Coverage = coverage(config);
     process.env.NODE_ENV = 'testing';
   });
 
-  describe('#all', function(){
+  describe('#all', function() {
 
     record_or_play('coverage/all');
 
-    it('exists as public method on Coverage', function(){
+    it('exists as public method on Coverage', function() {
       expect(Coverage).to.have.property('all');
     });
 
-    it('should throw InvalidRequestError when no param is passed', function(done){
+    it('should throw InvalidRequestError when no param is passed', function(done) {
       Coverage.all()
-        .catch(function(e){
+        .catch(function(e) {
           expect(e).to.be.an.instanceOf(errors.InvalidRequestError);
           done();
         });
     });
-    
-    it('should fetch coverage', function(done){
+
+    it('should fetch coverage', function(done) {
       Coverage.all({
-        "payer_id":"00001",
-        "provider_last_name":"Doe",
-        "provider_first_name":"John",
-        "provider_npi":"0123456789",
-        "member_id":"ZZZ445554301",
-        "member_first_name":"IDA",
-        "member_last_name":"FRANKLIN",
-        "member_dob":"1701-12-12",
-        "service_type":"30"
+        payer_id: '00001',
+        provider_last_name: 'Doe',
+        provider_first_name: 'John',
+        provider_npi: '0123456789',
+        member_id: 'ZZZ445554301',
+        member_first_name: 'IDA',
+        member_last_name: 'FRANKLIN',
+        member_dob: '1701-12-12',
+        service_type: '30',
       })
-      .then(function(coverage){
+      .then(function(coverage) {
         expect(coverage).to.be.an.instanceOf(Coverage);
         done();
       })
-      .catch(function(e){
+      .catch(function(e) {
         done(e);
       });
     });
